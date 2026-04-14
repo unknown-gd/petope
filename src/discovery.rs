a03fcacadc7fc8e6e09428aa11909797a28501b4
Retro <44505837+dankmolot@users.noreply.github.com>
2026-04-14 19:26:28 +0300

    rewrite discovery server to use TcpListener

--- src/discovery.rs
+++ src/discovery.rs
@@ -1,8 +1,10 @@
 use std::{
     collections::HashMap,
-    io::{self, Error, ErrorKind},
-    net::{Ipv4Addr, SocketAddr, UdpSocket},
+    io::{self, Error, ErrorKind, Read, Write},
+    net::{Ipv4Addr, SocketAddr, TcpListener, TcpStream, UdpSocket},
     str::{self, FromStr},
+    sync::{Arc, Mutex},
+    thread,
 };
 
 use clap::Args;
@@ -15,47 +17,71 @@ pub struct DiscoveryArgs {
 
 pub fn main(args: DiscoveryArgs) {
     let socket = get_socket(args.port);
+    let nodes = Arc::new(Mutex::new(HashMap::new()));
 
+    for stream in socket.incoming() {
+        match stream {
+            Err(e) => {
+                eprintln!("incoming connection error: {}", e);
+            }
+            Ok(stream) => {
+                let addr = stream.peer_addr().unwrap();
+                let nodes = nodes.clone();
+                thread::spawn(move || {
+                    handle_connection(stream, addr, nodes)
+                        .unwrap_or_else(|e| eprintln!("handle connection: {e}"))
+                });
+            }
+        }
+    }
+}
+
+fn handle_connection(
+    mut stream: TcpStream,
+    addr: SocketAddr,
+    nodes: Arc<Mutex<HashMap<String, SocketAddr>>>,
+) -> io::Result<()> {
     let mut buf = [0; 1024];
-    let mut nodes: HashMap<String, SocketAddr> = HashMap::new();
-
     loop {
-        let (received, addr) = socket.recv_from(&mut buf).unwrap();
-        process(&socket, addr, &buf[..received], &mut nodes);
+        let received = stream.read(&mut buf)?;
+        process(&mut stream, addr, &buf[..received], &nodes)?;
     }
 }
 
 fn process(
-    socket: &UdpSocket,
+    stream: &mut TcpStream,
     addr: SocketAddr,
     buf: &[u8],
-    nodes: &mut HashMap<String, SocketAddr>,
-) {
+    nodes: &Mutex<HashMap<String, SocketAddr>>,
+) -> io::Result<()> {
     let Ok(mut data) = str::from_utf8(buf) else {
-        return;
+        return Ok(());
     };
 
     data = data.trim();
     if data.is_empty() {
-        return;
+        return Ok(());
     }
 
     let mut args = data.split("|");
-    let Some(command) = args.next() else { return };
+    let Some(command) = args.next() else {
+        return Ok(());
+    };
 
     println!("{} -> {}", addr, data);
 
     match command {
         // you may ask why \n at the end? so I can debug with netcat :)
         "ping" => {
-            socket.send_to(b"pong\n", addr).unwrap();
+            stream.write(b"pong\n")?;
         }
         "register" => {
             let Some(node_id) = args.next() else {
                 println!("{} forgot to send an id!", addr);
-                return;
+                return Ok(());
             };
 
+            let mut nodes = nodes.lock().unwrap();
             nodes.insert(String::from(node_id), addr);
             println!(
                 "now {} known as {:?} (total {}/{} nodes in map)",
@@ -68,28 +94,29 @@ fn process(
         "get" => {
             let Some(node_id) = args.next() else {
                 println!("{} forgot to send an id!", addr);
-                return;
+                return Ok(());
             };
 
+            let nodes = nodes.lock().unwrap();
             match nodes.get(node_id) {
                 Some(node_addr) => {
                     println!("node {:?} was found as {}", node_id, node_addr);
-                    socket
-                        .send_to(format!("found|{}\n", node_addr).as_bytes(), addr)
-                        .unwrap();
+                    stream.write(format!("found|{}\n", node_addr).as_bytes())?;
                 }
                 None => {
                     println!("node {:?} not found", node_id);
-                    socket.send_to("404\n".as_bytes(), addr).unwrap();
+                    stream.write("404\n".as_bytes())?;
                 }
             }
         }
         _ => {}
     };
+
+    Ok(())
 }
 
-fn get_socket(port: u16) -> UdpSocket {
-    let socket = UdpSocket::bind(get_addr(port).as_str()).unwrap();
+fn get_socket(port: u16) -> TcpListener {
+    let socket = TcpListener::bind(get_addr(port).as_str()).unwrap();
     println!(
         "listening discovery server on {}",
         socket.local_addr().unwrap()
