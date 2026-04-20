d6cccbee871c6a4e1079df7bd42e98b03142855d
Retro <44505837+dankmolot@users.noreply.github.com>
2026-04-20 17:03:34 +0300

    example with tun creation + ping replies + route to 10.0.0.101

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,69 @@
 use anyhow::Result;
-use clap::{Parser, Subcommand};
-use iroh::{Endpoint, endpoint::presets, protocol::Router};
-use iroh_ping::Ping;
-use iroh_tickets::{Ticket, endpoint::EndpointTicket};
-
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Cli {
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand, Debug)]
-enum Commands {
-    Receive,
-    Send { ticket: String },
-}
+use etherparse::{Icmpv4Slice, Icmpv4Type, IpNumber, NetSlice, PacketBuilder, SlicedPacket};
+use net_route::{Handle, Route};
+use tun_rs::DeviceBuilder;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
-    let cli = Cli::parse();
-    match cli.command {
-        Commands::Receive => run_receiver().await?,
-        Commands::Send { ticket } => {
-            let ticket = EndpointTicket::deserialize(&ticket)?;
-            run_sender(ticket).await?;
+    tokio::spawn(async {
+        tokio::signal::ctrl_c().await.unwrap();
+        println!("bye bye");
+        std::process::exit(0);
+    });
+
+    let device = DeviceBuilder::new()
+        .name("utun9")
+        .mtu(1000)
+        .ipv4("10.0.0.100", 32, None)
+        .layer(tun_rs::Layer::L3)
+        .build_async()?;
+
+    let handle = Handle::new()?;
+    let route = Route::new("10.0.0.101".parse()?, 32).with_ifindex(device.if_index()?);
+    handle.add(&route).await.expect("add route");
+
+    let mut buf = vec![0; 2048];
+    loop {
+        let recv = device.recv(&mut buf).await?;
+        let buf = &buf[..recv];
+
+        println!("{} -> {}", recv, device.send(buf).await?);
+
+        match SlicedPacket::from_ip(buf) {
+            Err(e) => println!("parse packet: {:?}", e),
+            Ok(packet) => match packet.net {
+                Some(NetSlice::Ipv4(s)) => {
+                    let ipheader = s.header();
+                    if ipheader.protocol() == IpNumber::ICMP {
+                        println!(
+                            "got icmp {} -> {}",
+                            ipheader.source_addr(),
+                            ipheader.destination_addr()
+                        );
+
+                        let icmp_slice = Icmpv4Slice::from_slice(s.payload().payload)?;
+                        match icmp_slice.icmp_type() {
+                            Icmpv4Type::EchoRequest(r) => {
+                                let builder = PacketBuilder::ipv4(
+                                    ipheader.destination(),
+                                    ipheader.source(),
+                                    ipheader.ttl(),
+                                )
+                                .icmpv4_echo_reply(r.id, r.seq);
+
+                                let payload = icmp_slice.payload();
+                                let mut result =
+                                    Vec::<u8>::with_capacity(builder.size(payload.len()));
+                                builder.write(&mut result, payload)?;
+
+                                device.send(&result).await?;
+                            }
+                            _ => {}
+                        };
+                    }
+                }
+                _ => {}
+            },
         }
     }
-
-    Ok(())
-}
-
-async fn run_receiver() -> Result<()> {
-    let endpoint = Endpoint::bind(presets::N0).await?;
-    endpoint.online().await;
-    let ping = Ping::new();
-    let ticket = EndpointTicket::new(endpoint.addr());
-    println!("{ticket}");
-
-    let _router = Router::builder(endpoint)
-        .accept(iroh_ping::ALPN, ping)
-        .spawn();
-
-    tokio::signal::ctrl_c().await?;
-    Ok(())
-}
-
-async fn run_sender(ticket: EndpointTicket) -> Result<()> {
-    let send_ep = Endpoint::bind(presets::N0).await?;
-    let send_pinger = Ping::new();
-    let rtt = send_pinger
-        .ping(&send_ep, ticket.endpoint_addr().clone())
-        .await?;
-    println!("ping took: {:?} to complete", rtt);
-    Ok(())
 }
